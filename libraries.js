60d205ba99772d7b5d23bafc80ecf63a4637ee2c
VIPOCONNECT <vipo.m1985@gmail.com>
2025-04-01 11:34:47 +0300

    Add files via upload

---
+++ libraries.js
@@ -0,0 +1,18 @@
+// קובץ זה מכיל קישורים לספריות החיצוניות הנדרשות לאתר
+// יש להוסיף את הקישורים הבאים לקובץ ה-HTML:
+
+/*
+<!-- Three.js - ספריית תלת-ממד -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+
+<!-- GSAP - ספריית אנימציות -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+
+<!-- Font Awesome - אייקונים -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+<!-- Google Fonts - Heebo -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+*/
